cfc2b76ac4a521f9121a70c07450e45bc0a2f620
SengSerany <cranyseng@gmail.com>
2022-03-28 13:29:12 +0200

    refactoring errors

--- controllers/main.js
+++ controllers/main.js
@@ -1,12 +1,12 @@
 require('dotenv');
 const jwt = require('jsonwebtoken');
-const CustomAPIError = require('../errors/custom-error');
+const { BadRequestError } = require('../errors');
 
 const login = async (req, res) => {
     const {username, password} = req.body;
 
     if(!username || !password){
-        throw new CustomAPIError('Please provide username or/and password', 400);
+        throw new BadRequestError('Please provide username or/and password');
     }
 
     // Just for demo, normally provided by DB
@@ -20,8 +20,6 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-    console.log(req.user);
-
     const luckyNumber =  Math.floor(Math.random()*100);
         
     res.status(200).json({msg: `Hello ${req.user.username},`, secret: `Here your authorized data, your lucky number is ${luckyNumber}`});
